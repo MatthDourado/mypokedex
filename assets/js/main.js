7181637e570f0be3604764c12e7186b24ec62d07
MatthDourado <matheusdourado2011@gmail.com>
2023-09-18 08:44:20 -0300

    novas adições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,141 @@
-// function convertTypeToLi(pokemonTypes){
-//    return pokemonTypes.map((typeSlot) =>`<li class="type">${type}</li>`)
-   
-// }
+const loadMoreButton = document.getElementById("loadmore");
+const pokemonList = document.getElementById("pokemonList");
+const pokeModal = document.getElementById('pokeModal'); 
 
+const maxPokemons = 151;
+const limit = 9;
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-    <span class="number">#${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-      <ol class="types">
-            ${pokemon.types.map((type)=>`<li class="type ${type}">${type}</li>`).join('')}
-            
+// pokeApi.getPokemons().then((pokemons = []) => {
+//   pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+//   // mesma coisa que a função a acima
+//   // const newList = pokemons.map((pokemon)=> convertPokemonToLi(pokemon))
+//   // const newHtml = newList.join('')
+//   //pegou a lista de pokemons,com o map,mapeou e converteu para uma lista de Li e depois com o join,fez as lista sempre impressas sem separador,todas juntas
+// });
 
-      </ol>
-      <img
-      
-        src="${pokemon.photo}"
-        alt="${pokemon.name}"
-      />
-    </div>
+function loadPokemon(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `<li class="pokemon ${pokemon.type}" onclick ='testModal(${pokemon.number})' ontouch='testModal(${pokemon.number})'>
+        
+  <span class="number">#${pokemon.number}</span>
+  <span class="name">${pokemon.name}</span>
+  <div class="detail">
+    <ol class="types">
+          ${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join("")}
+          
+  
+    </ol>
+    <img
+    
+      src="${pokemon.photo}"
+      alt="${pokemon.name}"
+    />
+  </div>
   </li>
   
-  `;
+  `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
+function testModal(number){
+  pokeApi.getPokemonMoreDetails(number).then((pokemonDetail = []) => {
+    pokeModal.innerHTML = `
+        <li class="pokemonOpen ${pokemonDetail.type}">
+            <span class="nameOpen">${pokemonDetail.name}</span>
+            <div id="divCloseButton">
+                <span>#${pokemonDetail.number}</span>
+                <button class="closeButton" onclick="fecharJanela()" ontouch="fecharJanela()">X</button>
+            </div>
+
+            <div class="typesContainer">
+                <div class="types">
+                <span>Types:</span>
+                ${pokemonDetail.types
+                  .map(
+                    (type) => `<span class="openType ${type}">${type}</span>`
+                  )
+                  .join("")}
+                </span>
+                </div>
+            </div>
+        
+            <div class="divSkills">
+                <div class="skills"> 
+                    <span>Skills:</span>                
+                    <span class="skillOpen ${pokemonDetail.type}">${
+                    pokemonDetail.skill1
+                    }</span>
+                    <span class="skillOpen ${pokemonDetail.type}">${
+                    pokemonDetail.skill2
+                    }</span>
+                </div>
+            </div>
+
+            <div class="pokemonImageOpenContainer">
+                <img class="pokemonImage" src="${pokemonDetail.photo}" 
+                alt="${pokemonDetail.name}" />
+            </div>
+
+            <div class="attributesContainer">
+                <h3 class="attributesHeader">Attributes:</h3>
+                <div class="attributesDiv">
+                <div class="innerAttribute">
+                    <span>Hp:</span>
+                    <span>${pokemonDetail.statValueHp}</span>
+                </div>
+
+                <div class="innerAttribute">
+                    <span>ATK:</span>
+                    <span>${pokemonDetail.statValueAtk}</span>
+                </div>
+
+                <div class="innerAttribute">
+                    <span>DEF:</span>
+                    <span>${pokemonDetail.statValueDef}</span>
+                </div>
+
+                <div class="innerAttribute">
+                    <span>Special-ATK:</span>
+                    <span>${pokemonDetail.statValueSatk}</span>
+                </div>
+
+                <div class="innerAttribute">
+                    <span>Special-DEF:</span>
+                    <span>${pokemonDetail.statValueSdef}</span>
+                </div>
+
+                <div class="innerAttribute">
+                    <span>Speed:</span>
+                    <span>${pokemonDetail.statValueSpd}</span>
+                </div>
+                </div>
+            </div>
+        </li>
+        `;
+  });
 }
 
-const pokemonList = document.getElementById("pokemonList");
+loadPokemon(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
 
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-  
-  // mesma coisa que a função a acima
-  // const newList = pokemons.map((pokemon)=> convertPokemonToLi(pokemon))
-  // const newHtml = newList.join('')
+  const qtdRecordNextPage = offset + limit;
 
-  //pegou a lista de pokemons,com o map,mapeou e converteu para uma lista de Li e depois com o join,fez as lista sempre impressas sem separador,todas juntas
+  if (qtdRecordNextPage >= maxPokemons) {
+    const newLimit = maxPokemons - offset;
+    loadPokemon(offset, newLimit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else 
+  {
+    loadPokemon(offset, limit);
+  }
 });
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,20 +1,55 @@
 const pokeApi = {};
 
 function convertPokeApiToPokeModel(pokeDetail) {
-  const pokemon = new Pokemon();
-  pokemon.number = pokeDetail.order.toString().padStart(3,'0')
-//   pokemon.number = pokeDetail.order;
-  pokemon.name = pokeDetail.name;
-  
-  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
-  const [type] = types;
-  pokemon.types = types;
-  pokemon.type = type;
+  const pokemon = {
+    number: pokeDetail.id,
+    name: pokeDetail.name,
+    photo: pokeDetail.sprites.other["official-artwork"].front_default,
+    types: pokeDetail.types.map((typeSlot) => typeSlot.type.name),
+    type: pokeDetail.types[0].type.name,
+    skills: pokeDetail.abilities.map((skillsSlot) => skillsSlot.ability.name),
+    stats: {
+      hp: pokeDetail.stats[0].base_stat,
+      atk: pokeDetail.stats[1].base_stat,
+      def: pokeDetail.stats[2].base_stat,
+      satk: pokeDetail.stats[3].base_stat,
+      sdef: pokeDetail.stats[4].base_stat,
+      spd: pokeDetail.stats[5].base_stat,
+    },
+  };
 
-  pokemon.photo = pokeDetail.sprites.other["official-artwork"].front_default;
+  [pokemon.skill1, pokemon.skill2] = pokemon.skills;
+  [
+    pokemon.statValueHp,
+    pokemon.statValueAtk,
+    pokemon.statValueDef,
+    pokemon.statValueSatk,
+    pokemon.statValueSdef,
+    pokemon.statValueSpd,
+  ] = Object.values(pokemon.stats);
 
   return pokemon;
 }
+
+// function (pokeDetail) {
+   
+//   const pokemon = new Pokemon();
+//   pokemon.number = pokeDetail.id.toString().padStart(3,'0')
+// //   pokemon.number = pokeDetail.order;
+//   pokemon.name = pokeDetail.name;
+  
+//   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+//   const [type] = types;
+//   pokemon.types = types;
+//   pokemon.type = type;
+
+//   pokemon.photo = pokeDetail.sprites.other["official-artwork"].front_default;
+
+//   pokemon.skills = pokeDetail.abilities.map((skillsSlot) => skillsSlot.ability.name);
+  
+//   pokemon.stats = 
+  
+// }
 pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
     .then((response) => response.json())
@@ -31,3 +66,14 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
 
     .catch((error) => console.error(error));
 };
+
+pokeApi.getPokemonMoreDetails =async (pokemonNumber) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`;
+  const response =await fetch(url);
+  const pokeDetail =await response.json();
+  const pokemonDetail = convertPokeApiToPokeModel(pokeDetail);
+
+  abriJanela();
+
+  return pokemonDetail;
+}
